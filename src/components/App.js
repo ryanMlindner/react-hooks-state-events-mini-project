638289bca31d2be9db37c675891afa154e92cb50
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-06-15 11:07:36 -0600

    formatting

--- src/components/App.js
+++ src/components/App.js
@@ -9,19 +9,23 @@ function App() {
   const [taskList, setTasks] = useState(TASKS);
   const [selectedCategory, setCategory] = useState("All");
 
+  //callback from TaskList
   function deleteTask(event) {
     const newTaskList = taskList.filter(task => task.text !== event.target.id)
     setTasks(newTaskList);
   }
 
+  //callback from CategoryFilter
   function filterCategory(category) {
     setCategory(category);
   }
 
+  //callback from NewTaskForm
   function createNewTask(taskData) {
     setTasks(taskList => [...taskList, taskData]);
   }
 
+  //format for display, no mutate
   function filterList() {
     let filteredList = taskList;
     if (selectedCategory !== "All") {
